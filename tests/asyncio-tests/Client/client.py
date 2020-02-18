32cb38b2e7007a6b7a8e7b3ae7d77291ff0a90e7
Paul Bunel <paulbunel34@gmail.com>
2020-02-18 19:22:47 +0100

    Ajout de l'envoi de dossier de manière récursive + correction de bugs

--- tests/asyncio-tests/Client/client.py
+++ tests/asyncio-tests/Client/client.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3.8
 import asyncio
 import os
+import re
+
+#Longueur du nom qui n'est pas la longueur binaire
 
 def encodeBuffer(message):
+    if type(message) == bytes:
+        print("\033[31mMessage : encodé\033[0m")
+    else:
+        print("\033[31mMessage : non encodé\033[0m")
     sizeBuffer_send = len(message).to_bytes(4, "big")
-    # print("\033[36mSize Buffer sent : ", int.from_bytes(sizeBuffer_send, "big"), "\033[0m")
+    print("\033[36mSize Buffer sent : ", int.from_bytes(sizeBuffer_send, "big"), "\033[0m")
 
     return sizeBuffer_send
 
@@ -14,17 +21,23 @@ def decodeBuffer(buffer):
 
     return sizeBuffer_received
 
+def extract_path(path):
+    newPath = re.search('(../)*(.*)', path).group(2)
+
+    return newPath
+
 async def send_file(path, writer):
+    writer.write(int(0).to_bytes(1, "big"))
     try:
         with open(path, "rb") as file:
             data = file.read()
             # nameFile = path.split('/')[-1].encode()
-            lenghtName = len(path).to_bytes(1, "big")
+        lenghtpath = len(path.encode()).to_bytes(1, "big")
             # print("lenghtName = ", lenghtName)
-            data = lenghtName + path.encode() + data
+        data = lenghtpath + path.encode() + data
     except IOError:
         print("Erreur ! Le fichier n'a pas pu être ouvert")
-        data = "Erreur ! Le fichier n'a pas pu être ouvert"
+        data = "Erreur ! Le fichier n'a pas pu être ouvert".encode()
     
     await send_message(writer, data)
 
@@ -48,43 +61,67 @@ async def receive_file(reader):
         pass
     data = data[(lenghtName+1):]
     print("file received check ; path2 = ", path2, " nameFile = ", nameFile)
+    path2 = extract_path(path2)
     with open(path2+nameFile, "wb") as file:
         file.write(data)
 
 async def send_message(writer, message):
     sizeBuffer_send = encodeBuffer(message)
     writer.write(sizeBuffer_send)
+    await writer.drain()
     # print(f'\033[36mMessage send : \033[93m{message}\033[0m')
-    try:
-        writer.write(message.encode())
-    except AttributeError:
-        writer.write(message)
+    i = 0
+    while len(message) > 0:
+        # print(message[:1024], "\n\n")
+        writer.write(message[:1024])
+        message = message[1024:]
+        i += 1
+        if i == 10:
+            await writer.drain()
+            i = 0
+        
+    await writer.drain()
 
 async def receive_message(reader):
     sizeBuffer_received = decodeBuffer(await reader.read(4))
     data = await reader.read(sizeBuffer_received)
     return data
 
+async def send_dir(pathDir, writer):
+    if os.path.isdir(pathDir):
+        tree = os.listdir(pathDir)
+        for fileordir in tree:
+            if os.path.isdir(pathDir+'/'+fileordir):
+                print("dir : ", pathDir+'/'+fileordir)
+                await send_dir(pathDir+'/'+fileordir, writer)
+            else:
+                print("file : ", pathDir+'/'+fileordir)
+                await send_file(pathDir+'/'+fileordir, writer)
+                await writer.drain()
+    else:
+        await send_file(pathDir, writer)
+
 
 async def main():
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+    reader, writer = await asyncio.open_connection('127.0.0.1', 50001)
     connexion = True
 
     while connexion:
         action = input("Voulez-vous envoyer un projet ou en télécharger un ? ")
         if action == "envoyer":
-            writer.write(int(0).to_bytes(1, "big"))
             path = input("Veuillez entrer le chemin du fichier à envoyer : ")
-            await send_file(path, writer)
+            await send_dir(path, writer)
         elif action == "telecharger":
             writer.write(int(1).to_bytes(1, "big"))
-            nameFile = input("Veuillez entrer le nom du fichier à télécharger : ")
-            await send_message(writer, nameFile)
+            await writer.drain()
+            nameDir = input("Veuillez entrer le nom du fichier à télécharger : ")
+            await send_message(writer, nameDir.encode())
             # print("send message check")
             await receive_file(reader)
-            print(f"{nameFile} successfully downloaded")
+            print(f"{nameDir} successfully downloaded")
         elif action == "quitter":
             writer.write(int(2).to_bytes(1, "big"))
+            await writer.drain()
             connexion = False
         else:
             print('Veuillez entrer "envoyer", "telecharger" ou "quitter" svp.')
--- tests/asyncio-tests/Serveur/serveur.py
+++ tests/asyncio-tests/Serveur/serveur.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3.8
 import asyncio
 import os
+import re
 
 def encodeBuffer(message):
     sizeBuffer_send = len(message).to_bytes(4, "big")
-    print("\033[91mSize Buffer sent : ", int.from_bytes(sizeBuffer_send, "big"), "\033[0m")
+    # print("\033[91mSize Buffer sent : ", int.from_bytes(sizeBuffer_send, "big"), "\033[0m")
     
     return sizeBuffer_send
 
@@ -14,13 +15,18 @@ def decodeBuffer(buffer):
     
     return sizeBuffer_received
 
+def extract_path(path):
+    newPath = re.search('(../)*(.*)', path).group(2)
+    
+    return newPath
+
 async def send_file(path, writer):
     try:
         with open(path, "rb") as file:
             data = file.read()
             # nameFile = path.split('/')[-1].encode()
-            lenghtName = len(path).to_bytes(1, "big")
-            data = lenghtName + path + data
+            lenghtPath = len(path).to_bytes(1, "big")
+            data = lenghtPath + path + data
     except IOError:
         print("Erreur ! Le fichier n'a pas pu être ouvert")
         data = "Erreur ! Le fichier n'a pas pu être ouvert"
@@ -29,21 +35,24 @@ async def send_file(path, writer):
 
 async def receive_file(reader):
     data = await receive_message(reader)
-    lenghtName = data[0]
-    path = data[1:lenghtName+1]
+    # print("data = ", data)
+    lenghtPath = data[0]
+    path = data[1:lenghtPath+1]
     path = path.decode()
     path = path.split('/')
     nameFile = path[-1]
     path2 = ""
     for i in path[0:-1]:
         path2 += i + '/'
+    path2 = extract_path(path2)
     try:
         os.makedirs(path2)
     except:
         pass
-    data = data[(lenghtName+1):]
+    data = data[(lenghtPath+1):]
+    # print(data)
     print("file received check ; path2 = ", path2, " nameFile = ", nameFile)
-    with open(nameFile, "wb") as file:
+    with open(path2+nameFile, "wb") as file:
         file.write(data)
 
 async def send_message(writer, message):
@@ -57,9 +66,30 @@ async def send_message(writer, message):
 
 async def receive_message(reader):
     sizeBuffer_received = decodeBuffer(await reader.read(4))
-    data = await reader.read(sizeBuffer_received)
+    data = bytearray()
+    chuncksize = 1024
+    while sizeBuffer_received > 0:
+        if sizeBuffer_received < chuncksize:
+            chuncksize = sizeBuffer_received
+        packet = await reader.read(chuncksize)
+        data += packet
+        sizeBuffer_received -= len(packet)
+        # if sizeBuffer_received >= 1024:
+            # data += await reader.read(1024)
+        # else:
+            # data += await reader.read(sizeBuffer_received)
+        # sizeBuffer_received -= 1024 #- len(data)
 
     return data
+
+async def send_dir(pathDir):
+    tree = os.listdir(pathDir)
+    for file in tree:
+        if os.path.isdir(file):
+            await send_dir(file)
+        else:
+            await send_file(file)
+
 
 async def handle_echo(reader, writer):
     connexion = True
@@ -68,10 +98,12 @@ async def handle_echo(reader, writer):
         action = int.from_bytes(action, "big")
         print("action = ", action)
         if action == 0:
+            print("Réception d'un fichier client")
             await receive_file(reader)
         elif action == 1:
+            print("Envoi d'un fichier au client")
             nameFile = await receive_message(reader)
-            print(f"\033[36mClient ask for {nameFile.decode()!r}")
+            print(f"\033[36mClient ask for {extract_path(nameFile.decode())!r}")
             await send_file(nameFile, writer)
         elif action == 2:
             connexion = False
@@ -82,9 +114,10 @@ async def handle_echo(reader, writer):
     print("Close the connection")
     writer.close()
 
+
 async def main():
     server = await asyncio.start_server(
-        handle_echo, '127.0.0.1', 8888)
+        handle_echo, '127.0.0.1', 50001)
 
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
